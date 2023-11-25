40208c19bb0964e926693d46c9eb6335da656fb1
root <root@DESKTOP-QS0QAE8>
2023-11-25 11:25:38 +0100

    err fix

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,3 @@
 export default function getSumOfHoods(initialNumber, expansion1989=89, expansion2019=19) {
-    if (initialNumber !== undefined){
-      return initialNumber + expansion1989 + expansion2019;
-    } else {
-      return
-    }
-  }
+  return initialNumber + expansion1989 + expansion2019;
+}
