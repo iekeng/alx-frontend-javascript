cbe856b741ef015eec629d744b1f31aa82c1daf8
root <root@DESKTOP-QS0QAE8>
2023-11-26 22:20:26 +0100

    err fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
 function appendToEachArrayValue(array, appendString) {
+  if (appendString !== undefined && array){
   const arr = [];
-  for (let item of array) {
+    for (let item of array) {
     let value = appendString + item;
     arr.push(value);
-}
-
+    }
+  }
   return arr;
 }
